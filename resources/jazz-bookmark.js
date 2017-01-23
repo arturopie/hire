355817eb4d4b8bd0d062f65cedece7a995bdb567
Arturo Pie <arturop@nulogy.com>
2017-01-23 12:34:55 -0500

    adds jazz-bookmark code

---
+++ resources/jazz-bookmark.js
@@ -0,0 +1,5 @@
+javascript: (function() {
+  var repoUrl = jQuery("#website").siblings("a").text().trim();
+  var candidateName = jQuery(".candidate-name-wrap").text().trim();
+  jQuery("<textarea style='font-size: 14pt; width: 900px' rows='1'>hire start '" + candidateName + "' " + repoUrl + "</textarea>").insertAfter(".candidate-name-wrap").select();
+})();
