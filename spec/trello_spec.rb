cbd1d2ff50e97e4d4fd5fb12fc6009b98a2e9e19
Arturo Pie <arturop@nulogy.com>
2017-01-23 16:30:15 -0500

    skip trello_spec (learning test for learning trello api) when the keys are not set

--- spec/trello_spec.rb
+++ spec/trello_spec.rb
@@ -1,22 +1,24 @@
 require 'spec_helper'
 require 'hire/trello'
 
-module Hire
-  describe Trello do
-    it "find trello card" do
-      trello = Hire::Trello.new(authorization)
+if ENV["CONSUMER_KEY"].present?
+  module Hire
+    describe Trello do
+      it "find trello card" do
+        trello = Hire::Trello.new(authorization)
 
-      result = trello.find_card("RGI4UVGn")
+        result = trello.find_card("RGI4UVGn")
 
-      expect(result).to be_present
-    end
+        expect(result).to be_present
+      end
 
-    def authorization
-      {
-        consumer_key: ENV["CONSUMER_KEY"],
-        consumer_secret: ENV["CONSUMER_SECRET"],
-        oauth_token: ENV["OAUTH_TOKEN"]
-      }
+      def authorization
+        {
+          consumer_key: ENV["CONSUMER_KEY"],
+          consumer_secret: ENV["CONSUMER_SECRET"],
+          oauth_token: ENV["OAUTH_TOKEN"]
+        }
+      end
     end
   end
 end
