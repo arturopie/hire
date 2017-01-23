a9f427db4328c272cb626c6ed4f5702e655a307f
Arturo Pie <arturop@nulogy.com>
2017-01-23 16:31:43 -0500

    rename `start` command to `start_review` command

--- resources/jazz-bookmark.js
+++ resources/jazz-bookmark.js
@@ -1,5 +1,5 @@
 javascript: (function() {
   var repoUrl = jQuery("#website").siblings("a").text().trim();
   var candidateName = jQuery(".candidate-name-wrap").text().trim();
-  jQuery("<textarea style='font-size: 14pt; width: 900px' rows='1'>hire start '" + candidateName + "' " + repoUrl + "</textarea>").insertAfter(".candidate-name-wrap").select();
+  jQuery("<textarea style='font-size: 14pt; width: 900px' rows='1'>hire start_review '" + candidateName + "' " + repoUrl + "</textarea>").insertAfter(".candidate-name-wrap").select();
 })();
